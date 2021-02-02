8692c25838c39f260b88f3fd10f3629393733e7f
pedritoeldulce <franceskolyperez@gmail.com>
2021-02-01 22:43:15 -0500

    Agregar estilos

--- src/app.py
+++ src/app.py
@@ -5,6 +5,11 @@ root = Tk()
 root.title("Calculator")
 
 
+root.geometry("260x180")
+root.iconbitmap("envase.ico")
+root.config(bg="blue")
+root.resizable(0,0)
+
 display = Entry(root)
 display.grid(row=1, columnspan=6, sticky=W+E) # sticky: indica que lo que que av a abarcar
 
@@ -46,36 +51,39 @@ def calculate():
         clear_display()
         display.insert(0,  "Error")
 
+# Font
+# font =tkFont.Font(family="Helvetica", size=16)
+
 # Numeric Buttons
-Button(root, text="1", command=lambda:get_numbers(1)).grid(row=2, column=0,sticky=W+E)
-Button(root, text="2", command=lambda:get_numbers(2)).grid(row=2, column=1,sticky=W+E)
-Button(root, text="3", command=lambda:get_numbers(3)).grid(row=2, column=2,sticky=W+E)
+Button(root, text="1", fg='green', width=6, height=2, command=lambda:get_numbers(1)).grid(row=2, column=0,sticky=W+E)
+Button(root, text="2", fg='green', width=6, height=2, command=lambda:get_numbers(2)).grid(row=2, column=1,sticky=W+E)
+Button(root, text="3", fg='green', width=6, height=2, command=lambda:get_numbers(3)).grid(row=2, column=2,sticky=W+E)
 
-Button(root, text="4", command=lambda:get_numbers(4)).grid(row=3, column=0,sticky=W+E)
-Button(root, text="5", command=lambda:get_numbers(5)).grid(row=3, column=1,sticky=W+E)
-Button(root, text="6", command=lambda:get_numbers(6)).grid(row=3, column=2,sticky=W+E)
+Button(root, text="4", fg='green', height=2, command=lambda:get_numbers(4)).grid(row=3, column=0,sticky=W+E)
+Button(root, text="5", fg='green', height=2, command=lambda:get_numbers(5)).grid(row=3, column=1,sticky=W+E)
+Button(root, text="6", fg='green', height=2, command=lambda:get_numbers(6)).grid(row=3, column=2,sticky=W+E)
 
-Button(root, text="7", command=lambda:get_numbers(7)).grid(row=4, column=0,sticky=W+E)
-Button(root, text="8", command=lambda:get_numbers(8)).grid(row=4, column=1,sticky=W+E)
-Button(root, text="9", command=lambda:get_numbers(9)).grid(row=4, column=2,sticky=W+E)
+Button(root, text="7", fg='green', height=2, command=lambda:get_numbers(7)).grid(row=4, column=0,sticky=W+E)
+Button(root, text="8", fg='green', height=2, command=lambda:get_numbers(8)).grid(row=4, column=1,sticky=W+E)
+Button(root, text="9", fg='green', height=2, command=lambda:get_numbers(9)).grid(row=4, column=2,sticky=W+E)
 
 # Operation Buttons
 
-Button(root, text="AC", command=lambda:clear_display()).grid(row=5,  column=0,sticky=W+E)
-Button(root, text="0", command=lambda:get_numbers(0)).grid(row=5,  column=1,sticky=W+E)
-Button(root, text="%", command=lambda:get_operations("%")).grid(row=5, column=2,sticky=W+E)
+Button(root, text="AC", height=2, command=lambda:clear_display()).grid(row=5,  column=0,sticky=W+E)
+Button(root, text="0", height=2, command=lambda:get_numbers(0)).grid(row=5,  column=1,sticky=W+E)
+Button(root, text="%", height=2, command=lambda:get_operations("%")).grid(row=5, column=2,sticky=W+E)
 
 
-Button(root, text="+", command=lambda:get_operations("+")).grid(row=2,  column=3, sticky=W+E)
-Button(root, text="-", command=lambda:get_operations("-")).grid(row=3,  column=3, sticky=W+E)
-Button(root, text="*", command=lambda:get_operations("*")).grid(row=4,  column=3, sticky=W+E)
-Button(root, text="/", command=lambda:get_operations("/")).grid(row=5,  column=3, sticky=W+E)
+Button(root, text="+", height=2, width=6, command=lambda:get_operations("+")).grid(row=2,  column=3, sticky=W+E)
+Button(root, text="-", height=2, command=lambda:get_operations("-")).grid(row=3,  column=3, sticky=W+E)
+Button(root, text="*", height=2, command=lambda:get_operations("*")).grid(row=4,  column=3, sticky=W+E)
+Button(root, text="/", height=2, command=lambda:get_operations("/")).grid(row=5,  column=3, sticky=W+E)
 
-Button(root, text="←",command=lambda:undo()).grid(row=2,  column=4, sticky=W+E, columnspan=2)
-Button(root, text="exp", command=lambda:get_operations("**")).grid(row=3,  column=4, sticky=W+E)
-Button(root, text="^2", command=lambda:get_operations("**2")).grid(row=3,  column=5, sticky=W+E)
-Button(root, text="(", command=lambda:get_operations("(")).grid(row=4,  column=4, sticky=W+E)
-Button(root, text=")", command=lambda:get_operations(")")).grid(row=4,  column=5, sticky=W+E)
-Button(root, text="=", command=lambda:calculate()).grid(row=5,  column=4, sticky=W+E, columnspan = 2)
+Button(root, text="←", height=2, width=6,command=lambda:undo()).grid(row=2,  column=4, sticky=W+E, columnspan=2)
+Button(root, text="exp", height=2, command=lambda:get_operations("**")).grid(row=3,  column=4, sticky=W+E)
+Button(root, text="^2", height=2, command=lambda:get_operations("**2")).grid(row=3,  column=5, sticky=W+E)
+Button(root, text="(", height=2, command=lambda:get_operations("(")).grid(row=4,  column=4, sticky=W+E)
+Button(root, text=")", height=2, command=lambda:get_operations(")")).grid(row=4,  column=5, sticky=W+E)
+Button(root, text="=", height=2, command=lambda:calculate()).grid(row=5,  column=4, sticky=W+E, columnspan = 2)
 
 root.mainloop()
